9c8f3fec628a5edee7b627178fabf4d054cb3086
VladimirR <Vova.ru0007@yandex.ru>
2022-08-30 14:16:58 +0500

    zadanie2

---
+++ C#8SEMINAR/Zad2/Program.cs
@@ -0,0 +1,60 @@
+﻿/* Задайте двумерный массив. Напишите программу,
+которая заменяет строки на столбцы. В случае, если это
+невозможно, программа должна вывести сообщение для
+пользователя. */
+
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 50);
+PrintArray(array);
+int count = SearchItemPosition(array);
+Console.WriteLine();
+ChangeArray(array);
+PrintArray(array);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] ChangeArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        for (int j = i; j < array.GetLength(1); j++)
+        {
+            int temp = array[i, j];
+            array[i, j] = array[j, i];
+            array[j, i] = temp;
+
+        }
+
+    }
+    return array;
+}
+
