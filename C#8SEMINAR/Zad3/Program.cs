21136a96fd53c76652f72531ab43804099b89ab8
VladimirR <Vova.ru0007@yandex.ru>
2022-08-30 17:14:20 +0500

    zadanie3

---
+++ C#8SEMINAR/Zad3/Program.cs
@@ -0,0 +1,97 @@
+﻿/*Составить частотный словарь элементов
+двумерного массива. Частотный словарь содержит
+информацию о том, сколько раз встречается элемент
+входных данных. */
+Console.Clear();
+Console.Write("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+int[] result = GetRowNUmber(array);
+SortArray(result);
+PrintResult(result);
+
+int[,] GetArray(int m, int n, int min, int max)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(min, max);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[] GetRowNUmber(int[,] array)
+{
+    int[] result = new int[array.GetLength(0) * array.GetLength(1)];
+    int count = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result[count] = array[i, j];
+            count++;
+        }
+    }
+    return result;
+}
+
+
+void SortArray(int[] result)
+{
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = i + 1; j < result.Length; j++)
+        {
+            if (result[i] > result[j])
+            {
+                int temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+}
+
+void PrintResult(int[] result)
+{
+    int count = 1;
+    int firstNumber = result[0];
+    for (int i = 1; i < result.Length; i++)
+    {
+        {
+            if (result[i] != firstNumber)
+            {
+                Console.WriteLine($"Число {firstNumber} встречается {count} раз. ");
+                firstNumber = result[i];
+                count = 1;
+            }
+            else
+            {
+                count++;
+            }
+        }
+    }
+    Console.WriteLine($"Число {firstNumber} встречается {count} раз. ");
+}
