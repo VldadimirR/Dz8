87cba6594a67f6549a046ccf450536b0f7d3548e
VladimirR <Vova.ru0007@yandex.ru>
2022-08-31 13:33:19 +0500

    zadanie58

---
+++ С#Homework/Zad58/Program.cs
@@ -0,0 +1,76 @@
+﻿/*
+Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18
+*/
+
+Console.Clear();
+
+Console.Write("Введите число строк 1-й матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число столбцов 1-й матрицы (и строк 2-й):");
+int columnsAndRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число столбцов 2-й матрицы: ");
+int secondColumn = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] firstMatrix = GetArray(rows,columnsAndRows);
+int[,] secondMatrix = GetArray(columnsAndRows, secondColumn);
+
+PrintArray(firstMatrix);
+Console.WriteLine();
+
+PrintArray(secondMatrix);
+Console.WriteLine();
+
+MultiplyMatrix(firstMatrix, secondMatrix);
+
+int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(m, n);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyMatrix(int[,] firstMatrix, int[,] secondMatrix)
+{
+    int[,] resultMatrix = new int[firstMatrix.GetLength(0), secondMatrix.GetLength(1)];
+    for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int sum = 0;
+            for (int k = 0; k < firstMatrix.GetLength(1); k++)
+            {
+                sum += firstMatrix[i, k] * secondMatrix[k, j];
+            }
+            resultMatrix[i, j] = sum;
+        }
+    }
+    PrintArray(resultMatrix);
+}
+
